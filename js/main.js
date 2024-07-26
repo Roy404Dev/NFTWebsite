c307090824ba2702f70259557432c9fcb08b1ee8
Roy <rojusbalciunas@gmail.com>
2024-07-26 15:27:45 +0300

    setup

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import '../sass/main.scss';
+
+const app = {
+  init: () => {
+    document.addEventListener('DOMContentLoaded', app.load);
+  },
+  load: () => {
+    app.getdata();
+  },
+  getData: () => {
+
+  }
+}
